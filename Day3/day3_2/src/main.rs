16f1ae9ae931fcaefba42ae0fae1bf374f49065f
Rogelio Lopez <roge@Rogelios-MBP.lan>
2023-05-25 18:13:12 -0500

    Finished Day 3

--- Day3/day3_2/src/main.rs
+++ Day3/day3_2/src/main.rs
@@ -7,44 +7,61 @@ fn main() {
 
     let input_vec: Vec<&str> = input_as_text.split('\n').collect();
 
+    let mut total: u32 = 0;
+
     for i in (0..(input_vec.len() - 3)).step_by(3) {
-        let mut v_1: Vec<char> = input_vec[i].chars().collect();
+        let v_1: Vec<char> = input_vec[i].chars().collect();
         let v_2: Vec<char> = input_vec[i + 1].chars().collect();
         let v_3: Vec<char> = input_vec[i + 2].chars().collect();
 
-        v_1.extend(v_2);
-        v_1.extend(v_3);
-
-        order_vec(v_1);
+        total = total + find_key_val(v_1, v_2, v_3);
     }
+
+    println!("Total: {}", total);
 }
 
-fn order_vec(unordered_vec: Vec<char>) -> Vec<char> {
-    let mut ordered_vec: Vec<char> = unordered_vec;
+fn find_key_val(v_1: Vec<char>, v_2: Vec<char>, v_3: Vec<char>) -> u32 {
+    let max_len: usize = if v_2.len() >= v_3.len() {
+        v_2.len()
+    } else {
+        v_3.len()
+    };
 
-    for i in 0..(ordered_vec.len() - 1) {
-        for j in 1..ordered_vec.len() {
-            if ordered_vec[i] > ordered_vec[j] {
-                let temp: char = ordered_vec[i];
-                ordered_vec[i] = ordered_vec[j];
-                ordered_vec[j] = temp;
+    for i in v_1 {
+        let mut v2_found: bool = false;
+        let mut v3_found: bool = false;
+
+        for j in 0..(max_len) {
+            if v_2.get(j) != None {
+                if v_2[j] == i {
+                    v2_found = true;
+
+                    if v2_found && v3_found {
+                        return get_letter_score(v_2[j]);
+                    }
+                }
+            }
+            if v_3.get(j) != None {
+                if v_3[j] == i {
+                    v3_found = true;
+
+                    if v2_found && v3_found {
+                        return get_letter_score(v_3[j]);
+                    }
+                }
             }
         }
     }
-
-    return ordered_vec;
-}
-
-fn find_key_val(vect: Vec<char>) -> u32 {
-    if vect.len() > 3 {
-        for i in 0..(vect.len() - 3) {
-            if vect[i] == vect[i + 1] && vect[i + 1] == vect[i + 2] {
-                return 1;
-            }
-        }
-    }
-
     return 0;
 }
 
-//fn char_to_dec(letter: char) -> u32{}
+fn get_letter_score(letter: char) -> u32 {
+    let char_as_num: u32 = letter as u32;
+
+    if letter.is_uppercase() && letter != ' ' {
+        return char_as_num - 38;
+    } else if !letter.is_uppercase() && letter != ' ' {
+        return char_as_num - 96;
+    }
+    return 0;
+}
